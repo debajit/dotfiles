3efac9a0c3029e0b0f5698fe6958f050a90d6dad
Debajit Adhikary <debajit@debajit.com>
2020-11-09 18:10:54 -0800

    Add aliases for rsync backups

--- .aliases-functions.sh
+++ .aliases-functions.sh
@@ -1,3 +1,14 @@
+# Do a dry rsync and show what changes would be done (if files are
+# also deleted on the remote)
+#
+# Related:
+# - rsyncdir
+# - rcopydir
+#
+dry-rsyncdir() {
+  rsync -niaz --delete-after "${1}" "${2}" | egrep -v '^\.'
+}
+
 # Detect the volume level in the given audio file. Look at the
 # max_volume value here. If you increase the volume level of this
 # audio file, this max_volume value will change.
