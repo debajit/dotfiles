aef383b827adcb8f87e3897a09e5a3884d27b4b2
Debajit Adhikary <debajit@debajit.com>
2021-09-19 13:09:20 -0700

    Remove rsync aliases that are now redundant

--- .aliases-functions.sh
+++ .aliases-functions.sh
@@ -5,16 +5,6 @@ o() { open "${1:-.}" }
 t() { tree -C "${1:-.}" | less -R }
 tt() { tree -C -L 2 "${1:-.}" | less -R }
 
-# Do a dry rsync and show what changes would be done (if files are
-# also deleted on the remote)
-#
-# Related:
-# - backup
-# - syncdir
-#
-drybackup()  { rsync -niaz "${1}" "${2}" | egrep -v '^\.' | less }
-drysyncdir() { rsync -niaz --delete-after "${1}" "${2}" --exclude=".*" | egrep -v '^\.' | less }
-
 # Detect the volume level in the given audio file. Look at the
 # max_volume value here. If you increase the volume level of this
 # audio file, this max_volume value will change.
