411f1e180a074137ca1540d28bffb5ea295407d7
Debajit Adhikary <debajit@debajit.com>
2023-01-10 23:30:51 -0800

    Add ZSH alias suffix for .cpp and .cc files

--- .aliases-suffix.sh
+++ .aliases-suffix.sh
@@ -1,5 +1,6 @@
 alias -s {exs,gitconfig,rb,txt,yml}='bat'
 alias -s {flac,mp3,m4a,m4v,mp4,wav}='mpv'
+alias -s {cpp,cc}="g++ -std=c++20"          # This will generate a.out
 
 alias -s csv='vd'
 alias -s local='bat -l bash'
