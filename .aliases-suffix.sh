8581bf3622f42ddf747cc9956ed8d10c235da08e
Debajit Adhikary <debajit@debajit.com>
2023-03-24 12:49:28 -0700

    zsh: Open mkv and m4b with mpv by default

--- .aliases-suffix.sh
+++ .aliases-suffix.sh
@@ -1,5 +1,5 @@
 alias -s {exs,gitconfig,rb,txt,yml}='bat'
-alias -s {flac,mp3,m4a,m4v,mp4,wav}='mpv'
+alias -s {flac,mp3,m4a,m4b,m4v,mkv,mp4,wav}='mpv'
 alias -s {cpp,cc}="g++ -std=c++20"          # This will generate a.out
 
 alias -s csv='vd'
