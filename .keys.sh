89a7ad3ccfa585184f786b919f87b5cdb60aacc6
Debajit Adhikary <debajit@debajit.com>
2023-06-05 17:52:08 -0700

    Simplify and update keybindings

--- .keys.sh
+++ .keys.sh
@@ -1,13 +1,16 @@
 # Shell operations
 bindkey -s '^[R' 'source ~/.zshrc\n'                       # Alt+Shift+r  =>  Reload zsh configuration
 
-# Navigating between directories
-bindkey -s '^[U' 'cd ..\n'                                 # Alt+Shift+u  =>  cd ..
-bindkey -s '^[_' 'cd -\n'                                  # Alt+Shift+-  =>  cd -    (Previous directory)
+# Single-key shortcuts. (Ensure that these do not conflict with the
+# shell’s Emacs-style Meta keybindings you care about).
+bindkey -s '^[l' 'lt\n'                                    # Alt+l  =>  ls
+bindkey -s '^[u' 'cd ..\n'                                 # Alt+u  =>  cd ..
+bindkey -s '^[-' 'cd -\n'                                  # Alt+-  =>  cd -    (Previous directory)
+bindkey -s '^[j' 'zi\n'                                    # Alt+j  =>  zi      (zoxide recent directory picker)
 
 # Text insertion
 bindkey -s '^[C' 'cd '                                     # Alt+Shift+c  =>  Insert text: “cd ”
-bindkey -s '^[D' '~/.'                                     # Alt+Shift+d  =>  Insert text: “~/.”
+bindkey -s '^[D' 'cd Downloads\n'                          # Alt+Shift+d  =>  Insert text: “~/.”
 bindkey -s '^[G' ' | rg -S '                               # Alt+Shift+g  =>  | rg -S     (grep with smart-case)
 bindkey -s '^[L' ' | less '                                # Alt+Shift+l  =>  | less      (page)
 
@@ -15,7 +18,7 @@ bindkey -s '^[L' ' | less '                                # Alt+Shift+l  =>  | 
 bindkey -s '^[H' 'cd\n'                                    # Alt+Shift+h  =>  cd
 bindkey -s '^[T' 'cd /tmp\n'                               # Alt+Shift+t  =>  cd /tmp
 bindkey -s '^[W' 'cd ~/Projects/Code/debajit.com-hugo/\n'  # Alt+Shift+w  =>  Website
-bindkey -s '^[J' 'zi\n'                                    # Alt+Shift+j  =>  zi          (zoxide directory picker)
 
-# Programs
+# Commands
 bindkey -s '^[B' 'backup'                                  # Alt+Shift+b  =>  backup
+bindkey -s '^[S' 'espanso restart\n'                       # Alt+Shift+s  =>  espanso restart
