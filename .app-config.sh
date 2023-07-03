4a32c8c7e85951a4b0e8c5e27370c2e7c7e9ad70
Debajit Adhikary <debajit@debajit.com>
2023-07-03 12:24:27 -0700

    fzf: Use Rose Pine Moon theme

--- .app-config.sh
+++ .app-config.sh
@@ -16,6 +16,24 @@ case "$OSTYPE" in
     ;;
 esac
 
+# # Rose Pine theme for fzf
+# # https://github.com/rose-pine/fzf/blob/main/dist/rose-pine.sh
+# export FZF_DEFAULT_OPTS="
+#     --color=fg:#908caa,bg:#191724,hl:#ebbcba
+#     --color=fg+:#e0def4,bg+:#26233a,hl+:#ebbcba
+#     --color=border:#403d52,header:#31748f,gutter:#191724
+#     --color=spinner:#f6c177,info:#9ccfd8,separator:#403d52
+#     --color=pointer:#c4a7e7,marker:#eb6f92,prompt:#908caa"
+
+# Rose Pine Moon theme for fzf.
+# https://github.com/rose-pine/fzf/blob/main/dist/rose-pine-moon.sh
+export FZF_DEFAULT_OPTS="
+    --color=fg:#908caa,bg:#232136,hl:#ea9a97
+    --color=fg+:#e0def4,bg+:#393552,hl+:#ea9a97
+    --color=border:#44415a,header:#3e8fb0,gutter:#232136
+    --color=spinner:#f6c177,info:#9ccfd8,separator:#44415a
+    --color=pointer:#c4a7e7,marker:#eb6f92,prompt:#908caa"
+
 #
 # zoxide
 # Directory autojump based on frecency.
