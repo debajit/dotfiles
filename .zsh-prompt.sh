ebe00f2831cf5afe5c1a2de757e5d37764814770
Debajit Adhikary <debajit@debajit.com>
2023-07-03 16:23:52 -0700

    Prompt: Use TrueColor and update colors (inspired by the Rose Pine Moon theme)

--- .zsh-prompt.sh
+++ .zsh-prompt.sh
@@ -1,9 +1,29 @@
 #
 # .zsh-prompt
 #
-# My ZSH prompt.
+# My ZSH prompt. Uses TrueColor.
+#
+# The prompt is designed to be aesthetically pleasing and effective,
+# especially if you use the terminal for extended periods of time. It
+# aims to lower the cognitive load at any time by using progressive
+# disclosure:
+#
+# - A simple prompt is rendered by default with the current working
+#   directory (without the username and hostname). If you’re logged in
+#   to a remote session, then the username and hostname are added
+#   automatically.
+#
+# - The git branch name is only shown if you’re inside a git
+#   repository. No git status is shown at any time, as it can be seen
+#   easily by pressing a keystroke (see keys.sh, which binds Super+s
+#   for this).
+#
+# - The emoji separator for the git information is easy to customize
+#   (seasonally, for instance) by commenting and uncommenting a line
+#   in this file.
 #
 # References:
+# - ★ https://zsh.sourceforge.io/Doc/Release/Prompt-Expansion.html
 # - https://arjanvandergaag.nl/blog/customize-zsh-prompt-with-vcs-info.html
 # - https://scriptingosx.com/2019/07/moving-to-zsh-06-customizing-the-zsh-prompt/
 # - http://zsh.sourceforge.net/Doc/Release/Prompt-Expansion.html
@@ -26,10 +46,10 @@ precmd() {
 
 # Format the vcs_info_msg_0_ variable
 # Format the vcs_info_msg_0_ variable
-# zstyle ':vcs_info:git:*' formats '🌱 %b'
+zstyle ':vcs_info:git:*' formats '🌱 %b'
 # zstyle ':vcs_info:git:*' formats '🍄 %b'
 # zstyle ':vcs_info:git:*' formats '🐙 %b'
-zstyle ':vcs_info:git:*' formats '🎃 %b'
+# zstyle ':vcs_info:git:*' formats '🎃 %b'
 # zstyle ':vcs_info:git:*' formats '👻 %b'
 
 
@@ -41,10 +61,12 @@ NEWLINE=$'\n'
 # See color code previews here: https://i.stack.imgur.com/Vn0V3.png
 
 # Default prompt
-export PROMPT='${NEWLINE}%B%F{87}%5~%f%B%F{187} ${vcs_info_msg_0_}%f%b${NEWLINE}%B%F{154}%(!.#.❯) %f%b'
+# Syntax documentation: https://zsh.sourceforge.io/Doc/Release/Prompt-Expansion.html
+# man zshmisc
+export PROMPT='${NEWLINE}%B%F{#5efcfc}%5~%f%B%F{#f6c177} ${vcs_info_msg_0_}%f%b${NEWLINE}%B%F{154}%(!.#.❯) %f%b'
 
 # Use extended prompt for remote sessions
 if [[ -n "$SESSION_TYPE" ]]; then
   # Long prompt: "user at host in dir :emoji: branch"
-  export PROMPT='${NEWLINE}%F{183}%n%f %F{250}at%f %F{215}%m%f %F{250}in%f %B%F{87}%5~%f%B%F{187} ${vcs_info_msg_0_}%f%b${NEWLINE}%B%F{154}%(!.#.❯) %f%b'
+  export PROMPT='${NEWLINE}%B%F{#43a0bc}%n%f%b %F{#b2afc6}at%f %B%F{#eb6f92}%m%f%b %F{#b2afc6}in%f %B%F{#5efcfc}%5~%f%B%F{#f6c177} ${vcs_info_msg_0_}%f%b${NEWLINE}%B%F{154}%(!.#.❯) %f%b'
 fi
