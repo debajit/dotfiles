090a2425aacf7a7644fd14f363312ccf6e1c3899
Debajit Adhikary <debajit@debajit.com>
2026-08-10 11:50:18 -0700

    .aliases-suffix: open .bookmark files on pressing Enter

--- zsh/.aliases-suffix.sh
+++ zsh/.aliases-suffix.sh
@@ -2,6 +2,7 @@ alias -s {exs,gitconfig,py,rb,txt,yml}='bat'
 alias -s {flac,mp3,m4a,m4b,m4v,mkv,mp4,wav}='mpv'
 alias -s {cpp,cc}="g++ -std=c++20"          # This will generate a.out
 
+alias -s bookmark='open-bookmark'
 alias -s csv='csvlens'
 alias -s json='less'
 alias -s local='bat -l bash'
